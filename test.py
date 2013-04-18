02f9909f555ffae178b5c5ed3bc6e5a861eaef72
Steven Wilson <stvn.wlsn@gmail.com>
2013-04-18 10:01:01 +0100

    change test method name

--- test.py
+++ test.py
@@ -10,14 +10,14 @@ import sort
 LIST = np.random.randint(1000, size=100)
 
 
-def test(list):
+def test_bubble_sort(list):
     copylist = copy.copy(list)
     sort.bubble_sort(copylist)
 
 
 def main():
     print LIST
-    test(LIST)
+    test_bubble_sort(LIST)
 
 
 if __name__ == '__main__':
